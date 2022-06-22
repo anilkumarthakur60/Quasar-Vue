4e4a125a64ebfcb7477652ef594a73941835bbcf
anil kumar thakur <anilkumarthakur60@gmail.com>
2022-06-22 20:20:56 +0545

    kjh

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <h1>hello</h1>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: "App",
-  components: {},
-};
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
-<style scope>
-h1 {
-  font-size: 30px;
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
